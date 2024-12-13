83b6c2b55921afe2e5d78439c5c2744f4868dcff
ChengxiX <Hanson.xcx@outlook.com>
2024-12-13 18:52:47 +0800

    operation almost done

--- src/models.hpp
+++ src/models.hpp
@@ -1,11 +1,41 @@
+#ifndef MODELS_HPP
+#define MODELS_HPP
+
+#include "kvdb.hpp"
 #include "randomdb.cpp"
 #include "seqdb.cpp"
 #include "kvdb.cpp"
 #include "seqdb.hpp"
 #include <string>
 #include <vector>
+#include <set>
 
 constexpr static const int INSTANCE_ID = 1;
+
+template<int max>
+struct binstring : std::string {
+    constexpr const static int max_size = max;
+    char* to_bin();
+    void from_bin(char* bin);
+    constexpr static const int bin_size();
+};
+
+template<int max_size>
+void binstring<max_size>::from_bin(char* bin) {
+    this(bin, bin + max_size);
+}
+
+template<int max_size>
+char* binstring<max_size>::to_bin() {
+    char* bin = new char[max_size];
+    std::copy(c_str(), c_str() + max_size, bin);
+    return bin;
+}
+
+template<int max_size>
+constexpr const int binstring<max_size>::bin_size() {
+    return max_size;
+}
 
 struct textcmp {
     bool operator()(const char* a, const char* b) const {
@@ -15,19 +45,19 @@ struct textcmp {
 
 namespace User {
     constexpr static const int max_str_len = 31;
-    using UserId = char[max_str_len];
+    using UserId = binstring<max_str_len>;
     struct UserInfo {
         UserId id;
-        char Username[max_str_len];
-        char Password[max_str_len];
+        binstring<max_str_len> Username;
+        binstring<max_str_len> Password;
         int Privilege;
     };
     RandomDB<UserId, textcmp, UserInfo> db("user", INSTANCE_ID, "data/", false);
     bool su(const std::string &id, const std::string &password = "", int privilege = 0);
-    bool passwd(const std::string &id, const std::string &new_password, const std::string &old_password = "", const std::string & staff = "guest");
+    bool passwd(const std::string &id, const std::string &new_password, const std::string & staff, int privilege, const std::string &old_password = "");
     bool useradd(const std::string &id, const std::string &password, const std::string &username, int privilege, int current_pri, const std::string &staff);
     bool Register(const std::string &id, const std::string &password, const std::string &username) {return useradd(id, password, username, 1, 2, "guest");}
-    bool Delete(const std::string &id);
+    bool Delete(const std::string &id, const std::string &staff, int privilege);
     bool check_id(const std::string &id);
     bool check_str(const std::string &id);
     bool check_name(const std::string &id);
@@ -36,30 +66,39 @@ namespace User {
 
 namespace Book {
     constexpr static const int max_isbn_len = 21;
-    using ISBN_T = char[max_isbn_len];
+    using ISBN_T = binstring<max_isbn_len>;
     constexpr static const int max_str_len = 61;
     using Price_T = long long; // 定点两位，整数是10位
     struct BookInfo {
         int id;
-        char ISBN[max_isbn_len];
-        char Title[max_str_len];
-        char Author[max_str_len];
-        char Keyword[max_str_len]; // keywords
+        binstring<max_isbn_len> ISBN;
+        binstring<max_str_len> Title;
+        binstring<max_str_len> Author;
+        binstring<max_str_len> Keyword; // keywords
         int Stock;
         Price_T price; 
     };
+    bool check_isbn(const std::string &isbn);
+    bool check_str(const std::string &str);
+    
     SeqDB<class BookInfo> db("book", INSTANCE_ID, "data/");
     RandomDB<ISBN_T, textcmp, int> isbn2id("book_isbn_index", INSTANCE_ID, "data/", false);
     KVDB<int, std::less<int>, max_str_len> title2id("book_title_index", INSTANCE_ID, "data/", false);
     KVDB<int, std::less<int>, max_str_len> author2id("book_author_index", INSTANCE_ID, "data/", false);
     KVDB<int, std::less<int>, max_str_len> keyword2id("book_keyword_index", INSTANCE_ID, "data/", false);
     // 以 [ISBN] 字典升序依次输出
-    std::vector<BookInfo> show_isbn(const std::string &isbn);
-    std::vector<BookInfo> show_title(const std::string &title);
-    std::vector<BookInfo> show_author(const std::string &author);
-    std::vector<BookInfo> show_keyword(const std::string &keyword);
-    int select(const std::string &isbn); // 返回书的id
-    bool modify(int book_id, const std::string &isbn = "", const std::string &title = "", const std::string &author = "", const std::string &keyword = "", Price_T price = -1);
+    std::vector<BookInfo> show_isbn(const std::string &isbn, int privilege, const std::string &staff);
+    std::vector<BookInfo> show_title(const std::string &title, int privilege, const std::string &staff);
+    std::vector<BookInfo> show_author(const std::string &author, int privilege, const std::string &staff);
+    std::vector<BookInfo> show_keyword(const std::string &keyword, int privilege, const std::string &staff);
+    int select(const std::string &isbn, int privilege); // 返回书的id
+    bool modify(const std::string & userid, int book_id, int privilege, const std::string &isbn = "", const std::string &title = "", const std::string &author = "", const std::string &keyword = "", Price_T price = -1);
+
+    namespace Keywords {
+        bool check(const std::string &kw);
+        std::set<std::string> get(const std::string &kw);
+        bool check_single_kw(const std::string &str);
+    }
 }
 
 namespace Deal {
@@ -74,20 +113,20 @@ namespace Deal {
         int quantity;
     };
     SeqDB<DealInfo> db("deal", INSTANCE_ID, "data/");
-    Book::Price_T buy(const std::string &isbn, int quantity);
-    Book::Price_T import(int book_id, int quantity, Book::Price_T total_cost);
+    Book::Price_T buy(const std::string & user, const std::string &isbn, int quantity, int privilege);
+    Book::Price_T import(const std::string & user, int book_id, int quantity, Book::Price_T total_cost, int privilege);
     std::string show_finance(int count = -1);
     void report_finance();  // 不一定是void，待定
-    
 }
 
 namespace Log {
     constexpr static const int max_info_len = 64;
     enum OpType {
         UserLogin,
+        UserLogout,
         UserAdd,
         UserDelete,
-        UserModify,
+        UserPW,
         BookAdd,
         BookModify,
         DealImport,
@@ -96,11 +135,24 @@ namespace Log {
     struct LogInfo {
         int id;
         OpType type;
-        char user[User::max_str_len];
+        binstring<User::max_str_len> user;
         int record_id;
-        char info[max_info_len];
+        binstring<max_info_len> info;
     };
     SeqDB<LogInfo> db("log", INSTANCE_ID, "data/");
+    KVDB<int, std::less<int>, User::max_str_len> index_db("op2log", INSTANCE_ID, "data/", false);
     void report_employee(); // 不一定是void，待定
-    void log(); // 不一定是void，待定
+    void getlog(); // 不一定是void，待定
+    void login(const std::string & userId);
+    void logout(const std::string & userId);
+    void useradd(const std::string & userId, const std::string & info);
+    void userdelete(const std::string & userId, const std::string & info);
+    void userpw(const std::string & userId, const std::string & info);
+    void showbook(const std::string & userId, const std::string & info, int book_id);
+    void bookadd(const std::string & userId, int book_id, const std::string & info);
+    void bookmodify(const std::string & userId, int book_id, const std::string & info);
+    void dealimport(const std::string & userId, int deal_id, const std::string & info);
+    void dealsale(const std::string & userId, int deal_id, const std::string & info);
 }
+
+#endif
--- src/operations.cpp
+++ src/operations.cpp
@@ -1,4 +1,5 @@
 #include "models.hpp"
+#include <string>
 #include <utility>
 
 bool User::check_id(const std::string &id) {
@@ -36,12 +37,16 @@ bool User::su(const std::string &id , const std::string &password, int privilege
     if (!check_id(id)) return false;
     if (!check_name(password)) return false;
     if (!check_pri(privilege)) return false;
-    char id_arr[max_str_len];
-    std::strcpy(id_arr, id.c_str());
-    auto res = db.get(id_arr);
+    auto res = db.get(binstring<max_str_len>(id));
     if (! res.first) {return false;}
-    if (privilege > res.second.second.Privilege) {return true;}
-    if (std::strcmp(res.second.second.Password, password.c_str()) == 0) {return true;}
+    if (privilege > res.second.second.Privilege) {
+        Log::login(id);
+        return true;
+    }
+    if (std::strcmp(res.second.second.Password.c_str(), password.c_str()) == 0) {
+        Log::login(id);
+        return true;
+    }
     return false;
 }
 
@@ -52,16 +57,14 @@ bool User::useradd(const std::string &id, const std::string &password, const std
     if (!check_pri(privilege)) return false;
     if (current_pri != 2) {
         if (!check_pri(current_pri)) return false;
+        if (current_pri < 3) return false;
     }
     if (privilege >= current_pri) return false;
-    std::pair<char[max_str_len], UserInfo> p;
-    std::strcpy(p.first, id.c_str());
-    std::strcpy(p.second.id, id.c_str());
-    std::strcpy(p.second.Password, password.c_str());
-    std::strcpy(p.second.Username, username.c_str());
-    p.second.Privilege = privilege;
+    std::pair<binstring<max_str_len>, UserInfo> p(id, UserInfo{id, binstring<max_str_len>(username),
+    binstring<max_str_len>(password), privilege});
     try {
         db.insert(p);
+        Log::useradd(staff, "add user " + id);
         return true;
     }
     catch (RandomDB<User::UserId, textcmp, UserInfo>::DuplicateException &e) {
@@ -69,46 +72,308 @@ bool User::useradd(const std::string &id, const std::string &password, const std
     }
 }
 
-bool User::passwd(const std::string &id, const std::string &new_password, const std::string &old_password, const std::string & staff) {
-
+bool User::passwd(const std::string &id, const std::string &new_password, const std::string & staff, int privilege, const std::string &old_password) {
+    if (!check_id(id)) return false;
+    if (!check_str(new_password)) return false;
+    if (!check_pri(privilege)) return false;
+    if (privilege < 1) return false;
+    if (privilege == 7) {
+        auto res = db.get(binstring<max_str_len>(id));
+        if (! res.first) {return false;}
+        res.second.second.Password = binstring<max_str_len>(new_password);
+        db.erase(res.second.first);
+        db.insert(res.second);
+        Log::userpw(staff, "change password of user " + id);
+        return true;
+    }
+    else {
+        if (!check_str(old_password)) return false;
+        auto res = db.get(binstring<max_str_len>(id));
+        if (! res.first) {return false;}
+        if (std::strcmp(res.second.second.Password.c_str(), old_password.c_str()) != 0) return false;
+        res.second.second.Password = binstring<max_str_len>(new_password);
+        db.erase(res.second.first);
+        db.insert(res.second);
+        Log::userpw(staff, "change password of user " + id);
+        return true;
+    }
 }
 
-bool User::Delete(const std::string &id) {
-
+bool User::Delete(const std::string &id, const std::string &staff, int privilege) {
+    if (!check_id(id)) return false;
+    if (privilege != 7) return false;
+    auto res = db.get(binstring<max_str_len>(id));
+    if (! res.first) {return false;}
+    db.erase(res.second.first);
+    Log::userdelete(staff, "delete user " + id);
+    return true;
 }
 
-std::vector<Book::BookInfo> Book::show_isbn(const std::string &isbn) {
-
+bool Book::check_isbn(const std::string &isbn) {
+    if (isbn.size() != 13) return false;
+    for (char c : isbn) {
+        if (c < 32 || c >= 127) return false;
+    }
+    return true;
 }
 
-std::vector<Book::BookInfo> Book::show_title(const std::string &title) {
-
+bool Book::check_str(const std::string &str) {
+    if (str.size() > max_str_len - 1) return false;
+    for (char c : str) {
+        if (c < 32 || c >= 127 || c == '\"') return false;
+    }
+    return true;
 }
 
-std::vector<Book::BookInfo> Book::show_author(const std::string &author) {
+bool Book::Keywords::check_single_kw(const std::string &str) {
+    if (str.size() > max_str_len - 1) return false;
+    if (str.size() == 0) return false;
+    for (char c : str) {
+        if (c < 32 || c >= 127 || c == '\"' || c == '|' || c == ' ') return false;
+    }
+    return true;
+}
 
+std::vector<Book::BookInfo> Book::show_isbn(const std::string &isbn, int privilege, const std::string &staff) {
+    if (!User::check_pri(privilege)) return {};
+    if (check_isbn(isbn)) return {};
+    if (privilege < 1) return {};
+    auto res = isbn2id.get(binstring<max_isbn_len>(isbn));
+    std::vector<BookInfo> ret;
+    if (res.first) {
+        ret.push_back(db[res.second.second]);
+    }
+    Log::showbook(staff, "show book with ISBN " + isbn, res.second.second);
+    return ret;
+}
+
+std::vector<Book::BookInfo> Book::show_title(const std::string &title, int privilege, const std::string &staff) {
+    if (!User::check_pri(privilege)) return {};
+    if (!check_str(title)) return {};
+    if (privilege < 1) return {};
+    auto res = title2id.find(binstring<max_str_len>(title));
+    std::vector<BookInfo> ret;
+    for (auto &p : res) {
+        ret.push_back(db[p.second]);
+    }
+    Log::showbook(staff, "show book with title " + title, -1);
+    return ret;
+}
+
+std::vector<Book::BookInfo> Book::show_author(const std::string &author, int privilege, const std::string &staff) {
+    if (!User::check_pri(privilege)) return {};
+    if (!check_str(author)) return {};
+    if (privilege < 1) return {};
+    auto res = author2id.find(binstring<max_str_len>(author));
+    std::vector<BookInfo> ret;
+    for (auto &p : res) {
+        ret.push_back(db[p.second]);
+    }
+    Log::showbook(staff, "show book with author " + author, -1);
+    return ret;
 }   
 
-std::vector<Book::BookInfo> Book::show_keyword(const std::string &keyword) {
-
+std::vector<Book::BookInfo> Book::show_keyword(const std::string &keyword, int privilege, const std::string &staff) {
+    // keyword
+    if (!User::check_pri(privilege)) return {};
+    if (!Keywords::check_single_kw(keyword)) return {};
+    if (privilege < 1) return {};
+    auto res = keyword2id.find(binstring<max_str_len>(keyword));
+    std::vector<BookInfo> ret;
+    for (auto &p : res) {
+        ret.push_back(db[p.second]);
+    }
+    Log::showbook(staff, "show book with keyword " + keyword, -1);
+    return ret;
 }
 
-int Book::select(const std::string &isbn) {
-
+int Book::select(const std::string &isbn, int privilege) {
+    if (!User::check_pri(privilege)) return -1;
+    if (privilege < 3) return -1;
+    if (!check_isbn(isbn)) return -1;
+    auto res = isbn2id.get(binstring<max_isbn_len>(isbn));
+    if (!res.first) {
+        db.push_back(BookInfo{db.size(), binstring<max_isbn_len>(isbn), binstring<max_str_len>(""), binstring<max_str_len>(""), binstring<max_str_len>(""), 0, 0});
+        isbn2id.insert(std::make_pair(binstring<max_isbn_len>(isbn), db.size() - 1));
+        return db.size() - 1;
+    }
+    return res.second.second;
 }
 
-bool Book::modify(int book_id, const std::string &isbn, const std::string &title, const std::string &author, const std::string &keyword, Price_T price) {
-
+bool Book::modify(const std::string & userid, int book_id, int privilege, const std::string &isbn, const std::string &title, const std::string &author, const std::string &keyword, Price_T price) {
+    if (book_id < 0 || book_id >= db.size()) return false;
+    if (!User::check_pri(privilege)) return false;
+    if (privilege < 3) return false;
+    if (title != "") {
+        if (!check_str(title)) return false;
+    }
+    if (author != "") {
+        if (!check_str(author)) return false;
+    }
+    if (keyword != "") {
+        if (!Keywords::check(keyword)) return false;
+    }
+    if (price != -1) {
+        if (price < 0) return false;
+    }
+    auto book = db[book_id];
+    if (isbn == book.ISBN) {
+        return false;
+    }
+    if (isbn != "") {
+        if (!check_isbn(isbn)) return false;
+        auto res = isbn2id.get(binstring<max_isbn_len>(isbn));
+        if (!res.first) {
+            return false;
+        }
+        isbn2id.erase(binstring<max_isbn_len>(book.ISBN));
+        res.second.first = binstring<max_isbn_len>(isbn);
+        isbn2id.insert(res.second);
+    }
+    if (title != "") {
+        title2id.erase(binstring<max_str_len>(book.Title), book_id);
+        book.Title = binstring<max_str_len>(title);
+        title2id.insert(binstring<max_str_len>(title), book_id);
+    }
+    if (author != "") {
+        author2id.erase(binstring<max_str_len>(book.Author), book_id);
+        book.Author = binstring<max_str_len>(author);
+        author2id.insert(binstring<max_str_len>(author), book_id);
+    }
+    if (keyword != "") {
+        auto kws = Keywords::get(book.Keyword);
+        for (auto &kw : kws) {
+            keyword2id.erase(binstring<max_str_len>(kw), book_id);
+        }
+        kws = Keywords::get(keyword);
+        book.Keyword = binstring<max_str_len>(keyword);
+        for (auto &kw : kws) {
+            keyword2id.insert(binstring<max_str_len>(kw), book_id);
+        }
+    }
+    if (price != -1) {
+        book.price = price;
+    }
+    db.update(book, book_id);
+    Log::bookmodify(userid, book_id, "modify book " + book.ISBN);
+    return true;
 }
 
-Book::Price_T Deal::buy(const std::string &isbn, int quantity) {
-
+bool Book::Keywords::check(const std::string &kw) {
+    if (kw.size() > max_str_len - 1) return false;
+    auto kws = get(kw);
+    if (kws.empty()) return false;
+    return true;
 }
 
-Book::Price_T Deal::import(int book_id, int quantity, Book::Price_T total_cost) {
+std::set<std::string> Book::Keywords::get(const std::string &kw) {
+    std::set<std::string> ret;
+    std::string tmp;
+    std::stringstream ss(kw);
+    while (std::getline(ss, tmp, '|')) {
+        if (! check_single_kw(tmp)) return {};
+        auto res = ret.insert(tmp);
+        if (!res.second) return {};
+    }
+    return ret;
+}
 
+Book::Price_T Deal::buy(const std::string & user, const std::string &isbn, int quantity, int privilege) {
+    if (!User::check_pri(privilege)) return -1;
+    if (privilege < 1) return -1;
+    if (!Book::check_isbn(isbn)) return -1;
+    if (quantity <= 0) return -1;
+    auto res = Book::isbn2id.get(binstring<Book::max_isbn_len>(isbn));
+    if (!res.first) return -1;
+    auto book = Book::db[res.second.second];
+    if (book.Stock < quantity) return -1;
+    book.Stock -= quantity;
+    Book::db.update(book, res.second.second);
+    db.push_back(DealInfo{db.size(), DealType::Sale, book.price, quantity});
+    Log::dealsale(user, res.second.second, "buy book " + isbn + " " + std::to_string(quantity));
+    return book.price * quantity;
+}
+
+Book::Price_T Deal::import(const std::string & user, int book_id, int quantity, Book::Price_T total_cost, int privilege) {
+    if (!User::check_pri(privilege)) return -1;
+    if (privilege < 3) return -1;
+    if (quantity <= 0) return -1;
+    if (total_cost < 0) return -1;
+    if (book_id < 0 || book_id >= Book::db.size()) return -1;
+    auto book = Book::db[book_id];
+    book.Stock += quantity;
+    Book::db.update(book, book_id);
+    db.push_back(DealInfo{db.size(), DealType::Import, total_cost, quantity});
+    Log::dealimport(user, book_id, "import book " + book.ISBN + " " + std::to_string(quantity));
+    return total_cost;
 }
 
 std::string Deal::show_finance(int count) {
-    
+    if (count == -1) {
+        count = db.size();
+    }
+    Book::Price_T in = 0, out = 0;
+    auto list = db.range(db.size() - count, db.size());
+    for (auto &deal : list) {
+        if (deal.type == DealType::Sale) {
+            in += deal.price;
+        }
+        else {
+            out += deal.price;
+        }
+    }
+    return "+ " + std::to_string(in) + " - " + std::to_string(out);
 }
+
+void Deal::report_finance() {
+}
+
+// namespace Log {
+void Log::login(const std::string &id) {
+    db.push_back(LogInfo{db.size(), OpType::UserLogin, id, -1, ""});
+}
+
+void Log::logout(const std::string &id) {
+    db.push_back(LogInfo{db.size(), OpType::UserLogout, id, -1, ""});
+}
+
+void Log::useradd(const std::string &userId, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::UserAdd, userId, -1, info});
+}
+
+void Log::userdelete(const std::string &userId, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::UserDelete, userId, -1, info});
+}
+
+void Log::userpw(const std::string &userId, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::UserPW, userId, -1, info});
+}
+
+void Log::showbook(const std::string &userId, const std::string &info, int book_id) {
+    db.push_back(LogInfo{db.size(), OpType::BookAdd, userId, book_id, info});
+}
+
+void Log::bookadd(const std::string &userId, int book_id, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::BookAdd, userId, book_id, info});
+}
+
+void Log::bookmodify(const std::string &userId, int book_id, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::BookModify, userId, book_id, info});
+}
+
+void Log::dealimport(const std::string &userId, int deal_id, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::DealImport, userId, deal_id, info});
+}
+
+void Log::dealsale(const std::string &userId, int deal_id, const std::string &info) {
+    db.push_back(LogInfo{db.size(), OpType::DealSale, userId, deal_id, info});
+}
+
+void Log::report_employee() {
+}
+
+void Log::getlog() {
+}
+
+
+
